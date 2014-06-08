a557baa37f7cdb5f0acd8dda787776d303be0558
laoneo <allon.moritz@digital-peak.com>
2014-06-08 20:53:28 +0200

    Added an advanced option to show the slider only on the landing page

--- mod_dpslider/mod_dpslider.php
+++ mod_dpslider/mod_dpslider.php
@@ -1,11 +1,16 @@
 <?php
 /**
- * @package		DPSlide
+ * @package		DPSlider
  * @author		Digital Peak http://www.digital-peak.com
  * @copyright	Copyright (C) 2012 - 2013 Digital Peak. All rights reserved.
  * @license		http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
-
 defined('_JEXEC') or die();
 
-require(JModuleHelper::getLayoutPath('mod_dpslider'));
+if ($params->get('show_on_start_page_only', '0') == '1' && JUri::getInstance()->toString(array(
+		'query'
+)) != '')
+{
+	return;
+}
+require (JModuleHelper::getLayoutPath('mod_dpslider'));
