49780ca6c561a26bc5f784ef7e8a38c5bf28146d
laoneo <allon.moritz@digital-peak.com>
2015-12-15 13:36:36 +0100

    Added new effects fade, cube and coverflow

--- mod_dpslider/tmpl/default.php
+++ mod_dpslider/tmpl/default.php
@@ -2,67 +2,71 @@
 /**
  * @package		DPSlider
  * @author		Digital Peak http://www.digital-peak.com
- * @copyright	Copyright (C) 2012 - 2015 Digital Peak. All rights reserved.
+ * @copyright	Copyright (C) 2012 - 2014 Digital Peak. All rights reserved.
  * @license		http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
-
 defined('_JEXEC') or die();
 
+JHtml::_('jquery.framework', false, null, false);
+
 $doc = JFactory::getDocument();
+$doc->addScript(JURI::root() . 'modules/mod_dpslider/libraries/slider/swiper.jquery.min.js');
+$doc->addStyleSheet(JURI::root() . 'modules/mod_dpslider/libraries/slider/swiper.min.css');
 
-JHtml::_('jquery.framework');
-
-$doc->addScript(JURI::root().'modules/mod_dpslider/libraries/slider/slider.min.js');
-$doc->addStyleSheet(JURI::root().'modules/mod_dpslider/libraries/slider/slider.min.css');
+$doc->addScriptDeclaration('jQuery(document).ready(function() {
+	var dpSlider' . $module->id . ' = new Swiper("#mod-dpslider-' . $module->id . '", {
+		direction: "horizontal",
+		calculateHeight: true,
+		speed: 1000,
+		paginationClickable: true,
+		nextButton: ".swiper-button-next",
+		prevButton: ".swiper-button-prev",
+		spaceBetween: 10,
+		slidesPerView: 1,
+		loop: true,
+		autoplay: ' . ($params->get('autoplay', '') ? $params->get('autoplay', '') : "''") . ',
+		pagination: "#mod-dpslider-pagination-container-' . $module->id . '",
+		centeredSlides: true,
+		effect: "' . $params->get('effect', 'slide') . '"
+		' . (!$params->get('loop', 1) ? ',onProgress: function(s, p){if(s.slides.length < 4 || s.slides.length -3 == s.activeIndex)s.stopAutoplay();}' : '' ) . '
+	});
+});');
 
 $width = $params->get('width');
 $height = $params->get('height');
-
 $folder = $params->get('folder');
 
-$doc->addScriptDeclaration('jQuery(document).ready(function() {
-	var dpSlider'.$module->id.' = jQuery("#mod-dpslider-'.$module->id.'").swiper({
-		mode: "horizontal", calculateHeight: true, speed: 1000,
-		loop: '.($params->get('loop', 1) == 1 ? 'true' : 'false').',
-		autoplay: '.($params->get('autoplay', '') ? $params->get('autoplay', '') : "''").',
-		pagination: "#mod-dpslider-pagination-container-'.$module->id.' .mod-dpslider-pagination"
-	});
-	jQuery("#mod-dpslider-pagination-container-'.$module->id.' .swiper-pagination-switch").click(function(){
-    	dpSlider'.$module->id.'.stopAutoplay();
-    	dpSlider'.$module->id.'.swipeTo(jQuery(this).index());
-    });
-	jQuery("#mod-dpslider-pagination-container-'.$module->id.' .mod-dpslider-lt").click(function(){
-    	dpSlider'.$module->id.'.stopAutoplay();
-    	dpSlider'.$module->id.'.swipePrev();
-    });
-	jQuery("#mod-dpslider-pagination-container-'.$module->id.' .mod-dpslider-gt").click(function(){
-    	dpSlider'.$module->id.'.stopAutoplay();
-    	dpSlider'.$module->id.'.swipeNext();
-    });
-});');
-
-//<div id="swiper-scrollbar-<php echo $module->id>" class="swiper-scrollbar"></div>
 echo $params->get('textbefore');
 ?>
 
-<div id="mod-dpslider-<?php echo $module->id?>" class="swiper-container" style="<?php echo (empty($height) ? '' : 'height:'.$height.';').(empty($width) ? '' : 'width:'.$width.';')?>">
-  <div class="swiper-wrapper">
-<?php 
-if (!empty($folder) && $folder != -1) {
-	foreach (JFolder::files(JPATH_BASE.'/images/dpslider/'.$folder) as $file) { ?>
-      <div class="swiper-slide"><img src="images/dpslider/<?php echo $folder.'/'.$file?>"></div>
-<?php 
+<div id="mod-dpslider-<?php echo $module->id?>" class="swiper-container" style="<?php echo (empty($height) ? '' : 'max-height:'.$height.';').(empty($width) ? '' : 'max-width:'.$width.';')?>">
+	<div class="swiper-wrapper">
+<?php
+if (! empty($folder) && $folder != - 1)
+{
+	foreach (JFolder::files(JPATH_BASE . '/images/dpslider/' . $folder) as $file)
+	{
+		?>
+		<div class="swiper-slide">
+			<img src="images/dpslider/<?php echo $folder.'/'.$file?>">
+		</div>
+<?php
 	}
 }
-for ($i = 0; $i < 10; $i++) {
-	$content = $params->get('slide'.$i, '');
-	if (empty($content)) {
+for ($i = 0; $i < 10; $i ++)
+{
+	$content = $params->get('slide' . $i, '');
+	if (empty($content))
+	{
 		continue;
 	}
-	echo '<div class="swiper-slide">'.$content.'</div>';
+	echo '<div class="swiper-slide">' . $content . '</div>';
 }
 ?>
-  </div>
+	</div>
+	<div class="mod-dpslider-pagination swiper-pagination swiper-pagination-clickable"
+		id="mod-dpslider-pagination-container-<?php echo $module->id?>"></div>
+	<div class="swiper-button-next swiper-button-grey"></div>
+	<div class="swiper-button-prev swiper-button-grey"></div>
 </div>
-<div class="mod-dpslider-pagination-container" id="mod-dpslider-pagination-container-<?php echo $module->id?>"><span class="mod-dpslider-lt">&lt;</span><span class="mod-dpslider-pagination"></span><span class="mod-dpslider-gt">&gt;</span></div>
 <?php echo $params->get('textafter')?>
